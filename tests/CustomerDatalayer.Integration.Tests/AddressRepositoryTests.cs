00271c41231ab30fe816471b91262c2ad1afd5a3
m.kalenishin <m.kalenishin@ylab.io>
2022-08-24 23:49:18 +0300

    fix: changed AddressRepositoryTests

--- tests/CustomerDatalayer.Integration.Tests/AddressRepositoryTests.cs
+++ tests/CustomerDatalayer.Integration.Tests/AddressRepositoryTests.cs
@@ -5,12 +5,15 @@ namespace CustomerDatalayer.Integration.Tests
 {
     public class AddressRepositoryTests
     {
+        public AddressRepositoryFixture Fixture => new AddressRepositoryFixture();
+
         [Fact]
         public void ShouldBeAbleToCreateAddressRepository()
         {
             var repository = new AddressRepository();
             Assert.NotNull(repository);
         }
+
 
         [Fact]
         public void ShouldBeAbleToCreateAddress()
@@ -29,5 +32,79 @@ namespace CustomerDatalayer.Integration.Tests
             };
             repository.Create(address); 
         }
+
+
+        [Fact]
+        public void ShouldBeAbleToReadAddress()
+        {
+            var repository = Fixture.CreateAddressRepository();
+            Assert.NotNull(repository.Read(repository.GetAddressId()));
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToUpdateAddress()
+        {
+            var repository = Fixture.CreateAddressRepository();
+            var addresses = new Addresses()
+            {
+                AddressId = repository.GetAddressId(),
+                CustomerId = repository.GetCustomerId(),
+                AddressLine1 = "AddressLine1",
+                AddressLine2 = "AddressLine2",
+                AddressType = "Billing",
+                City = "Ostin",
+                PostalCode = "77777",
+                AddrState = "Texas",
+                Country = "USA"
+            };
+            repository.Update(addresses);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToDeleteAddress()
+        {
+            Fixture.DeleteAll();
+            var repository = Fixture.CreateAddressRepository();
+
+            repository.Delete(1);
+        }
+
     }
+
+
+    public class AddressRepositoryFixture
+    {
+        public void DeleteAll()
+        {
+            var repository = new AddressRepository();
+            repository.DeleteAll();
+        }
+
+        public Addresses CreateMockAddress()
+        {
+            var address = new Addresses()
+            {
+                CustomerId = 4,
+                AddressLine1 = "AddressLine1",
+                AddressLine2 = "AddressLine2",
+                AddressType = "Billing",
+                City = "Ostin",
+                PostalCode = "77777",
+                AddrState = "Texas",
+                Country = "USA"
+            };
+            var addressRepository = new AddressRepository();
+            addressRepository.Create(address);
+            return address;
+        }
+
+        public AddressRepository CreateAddressRepository()
+        {
+            return new AddressRepository();
+        }
+    }
+
+
 }
